70c692cec959253d7070a16a46ecea555ac6ad3c
Ashish-sarvadhi <162983001+Ashish-sarvadhi@users.noreply.github.com>
2025-03-30 10:29:29 +0530

    Step 2: Handle single number input

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,4 +1,9 @@
-function add(numbers){
-    return 0;
-}
- module.exports = {add};
+function add(numbers) {
+    //handled empty string
+    if (numbers === "") return 0; 
+    //converted string to number
+    return parseInt(numbers); 
+  }
+  
+  module.exports = { add };
+  
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,3 +1,9 @@
 const { add } =  require('../src/stringCalculator.js');
 
 test('should return 0 for empty string', () => { expect(add("")).toBe(0); });
+
+test("should return the number itself when a single number is passed", () => {
+    expect(add("1")).toBe(1);
+    expect(add("5")).toBe(5);
+  });
+  
